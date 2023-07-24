24936cc1bd035fdeda79e36139077a487404ffd1
Luiz Fernando <luizfernandooliveira360@gmail.com>
2023-07-24 18:02:52 -0300

    commit

---
+++ DESAFIO 6 - ANATOMIA DE UMA DIVISAO/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Anatomia de uma divisão</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <main>
+        <h1>Anatomia de uma divisão</h1>
+         <form action="<?=$_SERVER["PHP_SELF"]?>" method="get">
+            <label for="dividendo">Dividendo:</label>
+            <input type="number" name="dividendo" id="dividendo">
+            <label for="divisor">Divisor:</label>
+            <input type="number" name="divisor" id="divisor">
+            <input type="submit" value="Calcular">
+        </form>
+    </main>
+    <section id="resultado">
+        <h2>Resultado da divisão: </h2>
+       <?php 
+            $dividendo = $_GET["dividendo"] ?? 0;
+            $divisor = $_GET["divisor"] ?? 1;
+
+            $resDiv =($dividendo / $divisor);
+            $resto = $dividendo % $divisor;
+
+            echo "<table><tr><td style='border-right:solid'>$dividendo</td><td style='border-bottom:solid'>$divisor</td></tr>";
+
+            echo "<tr><td>$resto</td><td>".(int)$resDiv."</td></tr></table>";
+       ?>
+
+    </section>
+</body>
+</html>
